876ab7e4b388d539dd1e0dcebf7d501beedf99f5
rohman0127 <rohmanoffice27@gmail.com>
2025-10-18 03:14:52 +0700

    add login with username

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,10 +9,17 @@
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
-        <!-- Email Address -->
+        <!-- Username -->
+        <div class="mt-4">
+            <x-input-label for="username" :value="__('Username')" />
+            <x-text-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required autofocus autocomplete="name" />
+            <x-input-error :messages="$errors->get('username')" class="mt-2" />
+        </div>
+
+        <!-- Email -->
         <div class="mt-4">
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-text-input id="username" class="block mt-1 w-full" type="text" name="email" :value="old('email')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
