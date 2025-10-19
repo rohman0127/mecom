add81c472fa9fb5c5d26f61d10c597f12daa9651
rohman0127 <rohmanoffice27@gmail.com>
2025-10-19 23:11:06 +0700

    edit profile

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,8 @@ Route::get('/reload-captcha', function () {
 Route::controller(AdminController::class)->group(function () {
     Route::get('/admin/logout', 'destroy')->name('admin.logout');
     Route::get('/admin/profile', 'profile')->name('admin.profile');
+    Route::get('/edit/profile', 'EditProfile')->name('edit.profile');
+    Route::post('/store/profile', 'StoreProfile')->name('store.profile');
     
 });
 
