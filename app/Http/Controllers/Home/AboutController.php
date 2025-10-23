3a4fffaa3f1bc799271dd6c1814ee790e59939fd
rohman0127 <rohmanoffice27@gmail.com>
2025-10-23 23:05:22 +0700

    add backend update about

---
+++ app/Http/Controllers/Home/AboutController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Home;
+
+use App\Models\About;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
+
+class AboutController extends Controller
+{
+     public function AboutPage(){
+        $aboutpage = About::find(1);
+        return view('admin.about_page.about_page_all',compact('aboutpage'));
+
+     } // End Method 
+
+     public function UpdateAbout(Request $request)
+    {
+        $about_id = $request->id;
+
+        if ($request->file('about_image')) {
+            $image = $request->file('about_image');
+            $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+
+            // Inisialisasi ImageManager (pakai GD driver)
+            $manager = new ImageManager(new Driver());
+
+            // Baca dan resize gambar
+            $img = $manager->read($image);
+            $img->resize(523, 605);
+
+            // Simpan file ke folder public/upload/home_about/
+            $path = public_path('upload/home_about/' . $name_gen);
+            $img->save($path);
+
+            $save_url = 'upload/home_about/' . $name_gen;
+
+            // Update database
+            About::findOrFail($about_id)->update([
+                'title'              => $request->title,
+                'short_title'        => $request->short_title,
+                'short_description'  => $request->short_description,
+                'long_description'   => $request->long_description,
+                'about_image'        => $save_url,
+            ]);
+
+            $notification = [
+                'message'    => 'About Page Updated with Image Successfully',
+                'alert-type' => 'success',
+            ];
+
+            return redirect()->back()->with($notification);
+        } else {
+            // Update tanpa upload gambar
+            About::findOrFail($about_id)->update([
+                'title'              => $request->title,
+                'short_title'        => $request->short_title,
+                'short_description'  => $request->short_description,
+                'long_description'   => $request->long_description,
+            ]);
+
+            $notification = [
+                'message'    => 'About Page Updated without Image Successfully',
+                'alert-type' => 'success',
+            ];
+
+            return redirect()->back()->with($notification);
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\Home\HomeSliderController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Home\AboutController;
+use App\Http\Controllers\Home\HomeSliderController;
 
 /*
 |--------------------------------------------------------------------------
@@ -50,7 +51,12 @@ Route::controller(AdminController::class)->group(function () {
 Route::controller(HomeSliderController::class)->group(function () {
     Route::get('/home/slide', 'HomeSlider')->name('home.slide');
     Route::post('/update/slider', 'UpdateSlider')->name('update.slider');
+});
 
+ // About Page All Route 
+Route::controller(AboutController::class)->group(function () {
+    Route::get('/about/page', 'AboutPage')->name('about.page'); 
+     Route::post('/update/about', 'UpdateAbout')->name('update.about'); 
 });
 
 
